aa1dbfbf9e6bcac9a964102b1f1d34c590743c6e
John Mertens <john.mertens@gmail.com>
2015-09-26 23:14:37 -0700

    Add a test for build_body_from_items
    This commit also include an update to enable the minitest/pride.

--- test/models/list_test.rb
+++ test/models/list_test.rb
@@ -8,4 +8,19 @@ class ListTest < ActiveSupport::TestCase
   def test_valid
     assert list.valid?
   end
+
+  def test_that_body_populates_from_items
+    item1 = Item.create(title: 'Fake title', url: 'http://url1', blurb: 'Blah blah blah.')
+    item2 = Item.create(title: 'Fake title 2', url: 'http://url2', blurb: 'Blah blah blah, again.')
+
+    expected_str = <<-JMM.strip
+[Fake title](http://url1)
+Blah blah blah.
+
+[Fake title 2](http://url2)
+Blah blah blah, again.
+    JMM
+
+    assert_equal list.build_body_from_items([item1.id, item2.id]), expected_str
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,13 +2,12 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
+require "minitest/pride"
+require "minitest/autorun"
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
 # require "minitest/rails/capybara"
-
-# Uncomment for awesome colorful output
-# require "minitest/pride"
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
