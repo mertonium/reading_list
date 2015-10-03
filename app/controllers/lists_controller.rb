e59c16a6e3da0588b669ecb55ba35b893601b453
John Mertens <john.mertens@gmail.com>
2015-10-02 23:18:02 -0700

    Add standard controller actions for Lists

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,37 @@ class ListsController < ApplicationController
     @list = List.new(state: List::DRAFT, subject: '[Eng Reading List]')
 
     if params[:item_ids].present?
-      @list.build_body_from_items(params[:item_ids].split(',').map(&:to_i))
+      @item_ids = params[:item_ids].split(',').map(&:to_i)
+      @list.build_body_from_items(@item_ids)
     end
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @lists = List.order("sent_at DESC")
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  private
+
+  def list_params
+    params.require(:list)
+      .permit(:subject, :body, :state, :sent_at, :item_ids)
   end
 end
